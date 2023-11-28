dbec20a77a8450aa4a688ff0d08698d983752498
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2023-11-28 21:21:55 +0200

    adding scripts
    add some logic to validate the input form

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-let username = document.querySelector("#username")
+let username = document.querySelector("#username").value;
+let email = document.querySelector("#email-inp").value;
+let pass = document.querySelector("#username").value;
+let confirm_pass = document.querySelector("#username").value;
+let login_btn = document.querySelector(".btn");
+login_btn.addEventListener("click", function() {
+    if (username == "") {
+        let div = document.createElement('div')
+        let span = document.createElement("span")
+        let textSpan = document.createTextNode("please enter the Name")
+        div.appendChild(span)
+        span.appendChild(textSpan)
+        document.querySelector(".username").appendChild(div)
+        div.classList.add("error")
+
+    } else if (username !== "") {
+        email.focuse()
+    }
+})
