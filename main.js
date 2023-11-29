a93f0df486c0854b4a40bfc40d22b1f975cd0e8f
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2023-11-29 14:44:11 +0200

    make a function that check the input fields

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let username = document.querySelector("#username").value;
-let email = document.querySelector("#email-inp").value;
-let pass = document.querySelector("#username").value;
-let confirm_pass = document.querySelector("#username").value;
+let Uname = document.querySelector("#username");
+let email = document.querySelector("#email-inp");
+let pass = document.querySelector("#username");
+let confirm_pass = document.querySelector("#username");
 let login_btn = document.querySelector(".btn");
+
 login_btn.addEventListener("click", function() {
+    checkUserName(username)
+    checkUserEmail(email)
+    document.querySelector("input").value = " ";
+
+})
+
+function checkUserName(username) {
+    username = Uname.value
     if (username == "") {
         let div = document.createElement('div')
         let span = document.createElement("span")
@@ -14,6 +23,26 @@ login_btn.addEventListener("click", function() {
         div.classList.add("error")
 
     } else if (username !== "") {
-        email.focuse()
+        console.log(username);
+        document.querySelector(".error").style.display = "none"
+            // username.value = " ";
     }
-})
+}
+
+function checkUserEmail(uemail) {
+    uemail = email.value
+    if (uemail == "") {
+        let div = document.createElement('div')
+        let span = document.createElement("span")
+        let textSpan = document.createTextNode("please enter the Email")
+        div.appendChild(span)
+        span.appendChild(textSpan)
+        document.querySelector(".mail").appendChild(div)
+        div.classList.add("error")
+
+    } else if (uemail !== "") {
+        console.log(uemail);
+        document.querySelector(".error").style.display = "none"
+            // Uemail.value = " ";
+    }
+}
